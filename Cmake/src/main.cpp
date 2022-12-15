529c5581fa8c6e48c0f9b0ac19dabf5bc4b03139
ZEL <1362848545@qq.com>
2022-12-15 17:04:24 +0800

    cmake 项目模板

--- Cmake/src/main.cpp
+++ Cmake/src/main.cpp
@@ -1,6 +1,13 @@
-#include "public.h"
+#include "zel.h"
+#include "mode.h"
 
 int main() {
+
+    char name[256];
+
+    STRCPY(name, sizeof(name), "hello world");
+
+    printf("%s\n", name);
 
 
     return 0;
