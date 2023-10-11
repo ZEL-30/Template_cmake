4b7bbef0f43f26a0f360d89107f8634b81650e16
ZEL <1362848545@qq.com>
2023-10-11 13:59:59 +0800

    cmake

--- qt-cmake/src/main.cpp
+++ qt-cmake/src/main.cpp
@@ -11,6 +11,10 @@
 #pragma comment(lib, "user32.lib")
 
 int main(int argc, char *argv[]) {
+
+    // 设置高DPI
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
     QApplication a(argc, argv);
     MainWindow   w;
     w.show();
